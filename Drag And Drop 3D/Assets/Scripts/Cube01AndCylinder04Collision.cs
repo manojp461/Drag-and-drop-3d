b7eee14a6ba05d21f19e29535677c41bb6400b30
Manoj P <manojp461@gmail.com>
2022-01-17 20:17:51 +0530

    Drag and Collision script created

---
+++ Drag And Drop 3D/Assets/Scripts/Cube01AndCylinder04Collision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube01AndCylinder04Collision : MonoBehaviour
+{
+    public GameObject cube01;
+    public GameObject sphere02;
+    public GameObject cube03;
+    public GameObject cylinder04;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Cylinder04")
+        {
+            cube01.SetActive(false);
+            cylinder04.SetActive(false);
+        }
+    }
+}
